decacf38af2aaa74a7f80d03d252bc9c2f57403c
Kate Sowles <katesowles@gmail.com>
2016-04-15 15:12:54 -0700

    kim and i are done with this ish

--- dayFive/scripts/articleView.js
+++ dayFive/scripts/articleView.js
@@ -1,6 +1,20 @@
 // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 var articleView = {};
 
+function putObjectTogether() {
+  var jsonObject = {};
+
+  // this pushes the stringified text to the bottom right corner of our new.html page
+  jsonObject.title = $('#article-title').val();
+  jsonObject.category = $('#article-category').val();
+  jsonObject.author = $('#article-author').val();
+  jsonObject.authorUrl = $('#article-author-url').val();
+  jsonObject.publishedOn = $('#article-published').val();
+  jsonObject.body = marked($('#text-body').val()); // converts the markup to HTML, this is the raw article markup
+
+  return jsonObject;
+}
+
 articleView.populateFilters = function() {
   $('article').each(function() {
     if (!$(this).hasClass('template')) {
@@ -63,28 +77,50 @@ articleView.setTeasers = function() {
 };
 
 articleView.initNewArticlePage = function() {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later.
 
-  // TODO: The new articles we create will be copy/pasted into our source data file.
-  // Set up this "export" functionality. We can hide it for now, and show it once we
-  // have data to export. Also, let's add focus event to help us select the JSON.
+  $('.tab-content').show();
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  // DONE: The new articles we create will be copy/pasted into our source data file. Set up this "export" functionality. We can hide it for now, and show it once we have data to export. Also, let's add focus event to help us select the JSON.
+
+  $('input, textarea').on('blur', function() {
+    $('#article-export label').css('display', 'block');
+  });
+
+    function render() {
+
+      var jsonTemp = putObjectTogether();
+      var aMarkOut = $('#articles');
+      var aJson = $('#article-export label');
+      var newArticle = new Article(jsonTemp);
+
+      // this pushes the markup'd format text to the bottom left corner of our new.html page
+      aMarkOut.empty();
+      aMarkOut.append(newArticle.toHtml())
+
+      aMarkOut.find('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
+
+      // this is making a JSON object from the jsonObject object, keys and values are already paired up
+      var jsonString = aJson.text(JSON.stringify(jsonTemp));
+
+    }
+
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
+
+  $('input, textarea').on('input', render);
 };
 
-articleView.create = function() {
-  // TODO: Set up a var to hold the new article we are creating.
-  // Clear out the #articles element, so we can put in the updated preview
+// DONE: Set up a var to hold the new article we are creating. Clear out the #articles element, so we can put in the updated preview
 
-  // TODO: Instantiate an article based on what's in the form fields:
+// DONE: Instantiate an article based on what's in the form fields:
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+// DONE: Use our interface to the Handblebars template to put this new article into the DOM:
 
-  // TODO: Activate the highlighting of any code blocks:
+// DONE: Activate the highlighting of any code blocks:
 
-  // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-};
-
+// DONE: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
 
 articleView.initIndexPage = function() {
   articleView.populateFilters();
@@ -92,4 +128,5 @@ articleView.initIndexPage = function() {
   articleView.handleAuthorFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
+  articleView.initNewArticlePage();
 };
